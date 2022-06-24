438f53a93bc8ed1a59b865b9c657dcb06245552a
gsofter <real.kingstar@outlook.com>
2022-06-24 07:14:51 -0500

    update wallet connection

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { web3Accounts, web3Enable, web3FromAddress, web3ListRpcProviders, web3UseRpcProvider } from '@polkadot/extension-dapp'
+
 function App() {
+
+  const handleClickConnect = async () => {
+    const allInjected = await web3Enable('my cool dapp')
+
+    const allAccounts = await web3Accounts();
+
+    console.log('allAccounts => ', allAccounts)
+    const injector = await web3FromAddress(allAccounts[0].address)
+
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClickConnect}> Connect </button>
       </header>
     </div>
   );
